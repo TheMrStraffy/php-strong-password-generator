89b7c067f201b49df1bbfeaac4477e86e5421e96
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-12-03 19:05:26 +0100

    [milestone1 completed]

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-    $letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    $nums = ['0','1','2','3','4','5','6','7','8','9'];
+
+
+    $letters = range('a','z');
+    $upperCaseLetters = range('A','Z');
+    $nums = range(0,9);
     $specialChars = ['!','?','&','%','$','<','>','^','+','-','*','/','(',')','[',']','{','}','@','#','_','='];
 
-  var_dump($letters);
 ?>
 
 <!DOCTYPE html>
@@ -12,14 +14,35 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <title>Password Generator</title>
 </head>
 <body>
-  <h1><?php
-  echo $letters[1];
-  echo $nums[1];
-  echo $specialChars[1];
-  ?></h1>
+
+  <div class="container">
+  <h1>Password Generator</h1>
+
+  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
+  <input type="number" name="pswlen" id="pswlen" value="">
+  <label for="pswlen">Lunghezza Password</label>
+  <button type="submit">Genera Password</button>
+</form>
+<?php 
+
+  $ALL_CHARS = array_merge($letters, $upperCaseLetters, $nums, $specialChars);
+  $newString = implode($ALL_CHARS);
   
+  if(!empty($_GET['pswlen']) && $_GET['pswlen'] <= 18){
+    $passwordGenerated = str_shuffle($newString);
+
+    echo substr($passwordGenerated,0,$_GET['pswlen']);
+  } else{
+    echo 'insert a number between 1 and 18';
+  }
+  
+  
+  
+?>
+  </div>
 </body>
 </html>
